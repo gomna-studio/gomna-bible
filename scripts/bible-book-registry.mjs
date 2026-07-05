789d6f728bd1c0eb5bbc66cf080a4af2a30b7e81
Gomna Studio <gomnastudio@gmail.com>
2026-07-05 12:13:27 +0900

    refactor(audio): support bible book registry

---
+++ scripts/bible-book-registry.mjs
@@ -0,0 +1,200 @@
+// 구약 39권 book registry.
+// 한국어 책 이름은 old_testament.js의 oldTestamentData.books[].name 원본 기준.
+// bookId와 bookSlug는 동일하게 유지한다.
+export const BOOKS = {
+  genesis: {
+    book: '창세기',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'genesis',
+  },
+  exodus: {
+    book: '출애굽기',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'exodus',
+  },
+  leviticus: {
+    book: '레위기',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'leviticus',
+  },
+  numbers: {
+    book: '민수기',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'numbers',
+  },
+  deuteronomy: {
+    book: '신명기',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'deuteronomy',
+  },
+  joshua: {
+    book: '여호수아',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'joshua',
+  },
+  judges: {
+    book: '사사기',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'judges',
+  },
+  ruth: {
+    book: '룻기',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'ruth',
+  },
+  '1samuel': {
+    book: '사무엘상',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: '1samuel',
+  },
+  '2samuel': {
+    book: '사무엘하',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: '2samuel',
+  },
+  '1kings': {
+    book: '열왕기상',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: '1kings',
+  },
+  '2kings': {
+    book: '열왕기하',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: '2kings',
+  },
+  '1chronicles': {
+    book: '역대상',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: '1chronicles',
+  },
+  '2chronicles': {
+    book: '역대하',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: '2chronicles',
+  },
+  ezra: {
+    book: '에스라',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'ezra',
+  },
+  nehemiah: {
+    book: '느헤미야',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'nehemiah',
+  },
+  esther: {
+    book: '에스더',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'esther',
+  },
+  job: {
+    book: '욥기',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'job',
+  },
+  psalms: {
+    book: '시편',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'psalms',
+  },
+  proverbs: {
+    book: '잠언',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'proverbs',
+  },
+  ecclesiastes: {
+    book: '전도서',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'ecclesiastes',
+  },
+  song: {
+    book: '아가',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'song',
+  },
+  isaiah: {
+    book: '이사야',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'isaiah',
+  },
+  jeremiah: {
+    book: '예레미야',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'jeremiah',
+  },
+  lamentations: {
+    book: '예레미야애가',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'lamentations',
+  },
+  ezekiel: {
+    book: '에스겔',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'ezekiel',
+  },
+  daniel: {
+    book: '다니엘',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'daniel',
+  },
+  hosea: {
+    book: '호세아',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'hosea',
+  },
+  joel: {
+    book: '요엘',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'joel',
+  },
+  amos: {
+    book: '아모스',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'amos',
+  },
+  obadiah: {
+    book: '오바댜',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'obadiah',
+  },
+  jonah: {
+    book: '요나',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'jonah',
+  },
+  micah: {
+    book: '미가',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'micah',
+  },
+  nahum: {
+    book: '나훔',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'nahum',
+  },
+  habakkuk: {
+    book: '하박국',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'habakkuk',
+  },
+  zephaniah: {
+    book: '스바냐',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'zephaniah',
+  },
+  haggai: {
+    book: '학개',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'haggai',
+  },
+  zechariah: {
+    book: '스가랴',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'zechariah',
+  },
+  malachi: {
+    book: '말라기',
+    testamentVariable: 'oldTestamentData',
+    bookSlug: 'malachi',
+  },
+};
